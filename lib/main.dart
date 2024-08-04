1b50626e2ee44c02a6d84ff0964002e7cc6b88db
Samuel <samuelketema01@gmail.com>
2024-08-04 23:06:48 +0300

    Firebase added to main widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mychat/firebase_options.dart';
 import 'package:mychat/screens/welcome_screen.dart';
 import 'package:mychat/screens/login_screen.dart';
 import 'package:mychat/screens/registration_screen.dart';
 import 'package:mychat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FlashChat()); // Replace with your app's main widget
+}
+
 
 class FlashChat extends StatelessWidget {
   @override
@@ -17,10 +26,10 @@ class FlashChat extends StatelessWidget {
       ),
       initialRoute: WelcomeScreen.id,
       routes: {
-        RegistrationScreen.id :(context) => RegistrationScreen(),
-        LoginScreen.id : (context) => LoginScreen(),
-        ChatScreen.id : (context) => ChatScreen(),
-        WelcomeScreen.id : (context) => WelcomeScreen()
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen()
       },
     );
   }
