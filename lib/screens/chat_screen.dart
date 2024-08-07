3c4ab5cb31a065b27b1bd7c82396d3aafd7c9eea
Samuel <samuelketema01@gmail.com>
2024-08-07 22:25:16 +0300

    Trying to push the QueryDocumentSnapshot data to the screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:mychat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -22,7 +24,7 @@ class _ChatScreenState extends State<ChatScreen> {
     getCurrentUser();
   }
 
-  void getCurrentUser()async{
+  void getCurrentUser() async {
     try {
       final user = await _auth.currentUser;
       if (user != null) {
@@ -33,12 +35,16 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void getMessages()async{
-    final messages = await _fireStore.collection('messages').get();
-    for (var message in messages.docs) {
-      print(message.data());
-    }
+  void messageStream() async {
+    final messages =
+        await FirebaseFirestore.instance.collection('messages').snapshots();
+    messages.listen((QuerySnapshot snapshot) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +54,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                getMessages();
+                messageStream();
                 //Implement logout functionality
               }),
         ],
@@ -60,7 +66,26 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                stream: _fireStore.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+                  if(!snapshot.hasData){
+                    return CircularProgressIndicator(color: Colors.white,);
+                  }
+                  final messages = snapshot.data?.docs;
+                  List<Widget> messageWidgets = [];
+                  for (var message in messages!) {
+                    final messageData = message.data() as Map<String, dynamic>;
+                    final messageText = messageData['text'];
+                    final messageSender = messageData['sender'];
 
+                    final messageWidget = Text('$messageText from $messageSender' );
+                    messageWidgets.add(messageWidget);
+                  }
+                  return Column(
+                    children: messageWidgets,
+                  );
+                }),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
